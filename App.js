a4e1bdef62de74ea3de968af0337f63aa9270040
Norio　Kaneko <akamt68@gmail.com>
2017-12-10 14:57:41 +0900

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from 'react-apollo';
 import TodoList from './TodoList';
 
 const link = new HttpLink({
-  uri: 'https://api.graph.cool/simple/v1/cj98a57z515m50115c8of9cu7',
+  uri: 'https://api.graph.cool/simple/v1/cjb0chguy1ufa0192csa9ouon',
 });
 const cache = new InMemoryCache();
 const client = new ApolloClient({ link, cache });
